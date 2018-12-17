76e15fd2efb0ed9f371aed0821f187ca2a108177
Traian Stanciu <stanciu.traian@gmail.com>
2018-12-17 16:35:43 +0200

    lookbook distortion cu forte noi :)

--- includes/gbt-blocks/lookbook_distortion/block.php
+++ includes/gbt-blocks/lookbook_distortion/block.php
@@ -32,30 +32,44 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
 		if ( $products ) :
 
 		?>
-			<div class="gbt_18_distorsion_lookbook <?php echo $align; ?>">
+			<div class="gbt_18_distorsion_lookbook">
 				<?php foreach( $products as $product ) : ?>
 
 					<section class="gbt_18_distorsion_lookbook_item">
-		                <div class="gbt_18_distorsion_image" data-displacement="<?php echo plugins_url( 'assets/images/animations/' . $animation . '.jpg', __FILE__ ); ?>" data-intensity="-0.65" data-speedIn="1.2" data-speedOut="1.2">
-		                	<?php 
-								$image = wp_get_attachment_image( $product->get_image_id(), 'square' );
-								$image = !$image? wc_placeholder_img() : $image;
+						<?php 
+							$image = wp_get_attachment_image_src( $product->get_image_id(), 'woocommerce_single' );
+							$iobj  = wp_get_attachment_image( $product->get_image_id(), 'woocommerce_single' );
 
-								$gallery_ids = $product->get_gallery_image_ids();
-	                			if( isset($gallery_ids[0]) ) {
-	                				$image2 = wp_get_attachment_image( $gallery_ids[0], 'square' );
-	                			}    
-	                			$image2 = !$image2? $image : $image2;           
-		    				?>
-								<?php 
-									echo $image;
-									echo $image2;
-								?>
-		                </div>
+							$gallery_ids = $product->get_gallery_image_ids();
+                			if( isset($gallery_ids[0]) ) {
+                				$image2 = wp_get_attachment_image_src( $gallery_ids[0], 'woocommerce_single' );
+                				$iobj2  = wp_get_attachment_image( $gallery_ids[0], 'woocommerce_single' );
+                			}   
+
+                			$distortion = false;
+                			if (isset($image) && isset($image2)) {
+                				if (isset($image[1]) && isset($image2[1]) && ($image[1] == $image2[1]) &&
+                					isset($image[2]) && isset($image2[2]) && ($image[2] == $image2[2])) { // images have same width and weight
+                					$distortion = true;
+                					$ratio = $image[1] / $image[2];
+                				}
+                			}
+						?>
+						<?php if ($distortion === true): ?>
+			                <div class="gbt_18_distorsion_image" style="width: 50%; height: 0; padding-bottom: calc(50% / <?php echo $ratio; ?>);" data-displacement="<?php echo plugins_url( 'assets/images/animations/' . $animation . '.jpg', __FILE__ ); ?>" data-intensity="-0.65" data-speedIn="1.2" data-speedOut="1.2">
+			                	<?php echo $iobj, $iobj2; ?>
+			                </div>
+		                <?php else : ?>
+							<div class="gbt_18_fade_images">
+								<?php echo isset($iobj)? $iobj : wc_placeholder_img(); ?>
+								<?php echo isset($iobj2)? $iobj2 : ''; ?>
+							</div>
+		                <?php endif; ?>
 		                <div class="gbt_18_distorsion_lookbook_content" style="background-color: <?php echo $bgColor; ?>;">
 		                    <div class="gbt_18_text_wrapper">
 		                        <h2 style="color:<?php echo $textColor; ?>"><?php echo $product->get_name(); ?></h2>
 		                        <p style="color:<?php echo $textColor; ?>"><?php echo $product->get_short_description(); ?></p>
+		                        <div class="gbt_18_price_button">
 		                        <span class="gbt_18_product_price" style="color:<?php echo $textColor; ?>">
 		                            <?php echo $product->get_price_html(); ?>
 		                        </span>
@@ -64,6 +78,7 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
 										href="<?php echo esc_url($product->add_to_cart_url()); ?>">
 										<?php echo $product->add_to_cart_text(); ?>
 								</a>
+								</div>
 		                    </div>
 		                </div>
 		            </section>
