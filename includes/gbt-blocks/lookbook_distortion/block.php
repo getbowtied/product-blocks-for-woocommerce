77dce19f4e2ea04ee617bcecda42a7f39f1ed2e8
Traian Stanciu <stanciu.traian@gmail.com>
2018-12-19 10:40:30 +0200

    lb distortion

--- includes/gbt-blocks/lookbook_distortion/block.php
+++ includes/gbt-blocks/lookbook_distortion/block.php
@@ -37,13 +37,13 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
 
 					<section class="gbt_18_distorsion_lookbook_item">
 						<?php 
-							$image = wp_get_attachment_image_src( $product->get_image_id(), 'woocommerce_single' );
-							$iobj  = wp_get_attachment_image( $product->get_image_id(), 'woocommerce_single' );
+							$image = wp_get_attachment_image_src( $product->get_image_id(), array(1000,1000) );
+							$iobj  = wp_get_attachment_image( $product->get_image_id(), array(1000,1000) );
 
 							$gallery_ids = $product->get_gallery_image_ids();
                 			if( isset($gallery_ids[0]) ) {
-                				$image2 = wp_get_attachment_image_src( $gallery_ids[0], 'woocommerce_single' );
-                				$iobj2  = wp_get_attachment_image( $gallery_ids[0], 'woocommerce_single' );
+                				$image2 = wp_get_attachment_image_src( $gallery_ids[0], array(1000,1000) );
+                				$iobj2  = wp_get_attachment_image( $gallery_ids[0], array(1000,1000) );
                 			}   
 
                 			$distortion = false;
@@ -59,24 +59,26 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
                 				if ($ratio >= 1) { // landscape image
                 					$c_height = 50;
                 					$c_width  = 50 * $ratio;
-                					$margins  = ($c_width - 50) / 2;
+                					$margins  = "margin: 0 " . -($c_width - 50) / 2 . "%";
                 				} else {
                 					$c_height = 50 / $ratio;
                 					$c_width = 50;
-                					$margins = 0;
+                					$margins = "margin: " . ($c_width - $c_height) / 2 . "% 0";
                 				}
                 			}
 						?>
-						<?php if ($distortion === true): ?>
-			                <div class="gbt_18_distorsion_image" style="margin: 0 -<?php echo $margins; ?>%; width: <?php echo $c_width; ?>%; height: 0; padding-bottom: <?php echo $c_height?>%;" data-displacement="<?php echo plugins_url( 'assets/images/animations/' . $animation . '.jpg', __FILE__ ); ?>" data-intensity="-0.65" data-speedIn="1.2" data-speedOut="1.2">
-			                	<?php echo $iobj, $iobj2; ?>
-			                </div>
-		                <?php else : ?>
-							<div class="gbt_18_fade_images">
-								<?php echo isset($iobj)? $iobj : wc_placeholder_img(); ?>
-								<?php echo isset($iobj2)? $iobj2 : ''; ?>
-							</div>
-		                <?php endif; ?>
+						<a href="<?php echo $product->get_permalink(); ?>">
+							<?php if ($distortion === true): ?>
+				                <div class="gbt_18_distorsion_image" style="<?php echo $margins; ?>; width: <?php echo $c_width; ?>%; height: 0; padding-bottom: <?php echo $c_height?>%;" data-displacement="<?php echo plugins_url( 'assets/images/animations/' . $animation . '.jpg', __FILE__ ); ?>" data-intensity="-0.65" data-speedIn="1.2" data-speedOut="1.2">
+				                	<?php echo $iobj, $iobj2; ?>
+				                </div>
+			                <?php else : ?>
+								<div class="gbt_18_fade_images">
+									<?php echo isset($iobj)? $iobj : wc_placeholder_img(); ?>
+									<?php echo isset($iobj2)? $iobj2 : ''; ?>
+								</div>
+			                <?php endif; ?>
+		            	</a>
 		                <div class="gbt_18_distorsion_lookbook_content" style="background-color: <?php echo $bgColor; ?>;">
 		                    <div class="gbt_18_text_wrapper">
 		                    	<div class="gbt_18_content_top">
@@ -87,7 +89,7 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
 			                        <span class="gbt_18_product_price" style="color:<?php echo $textColor; ?>">
 			                            <?php echo $product->get_price_html(); ?>
 			                        </span>
-									<a 		style="color:<?php echo $textColor; ?>" 
+									<a 		style="color:<?php echo $bgColor; ?>; background-color: <?php echo $textColor; ?>;" 
 											class="single_add_to_cart_button button alt" 
 											href="<?php echo esc_url($product->add_to_cart_url()); ?>">
 											<?php echo $product->add_to_cart_text(); ?>
