824ab8fc59571e115b59f714606a68ba41bd0228
Traian Stanciu <stanciu.traian@gmail.com>
2018-12-19 14:51:57 +0200

    ..

--- includes/gbt-blocks/lookbook_distortion/block.php
+++ includes/gbt-blocks/lookbook_distortion/block.php
@@ -115,7 +115,7 @@ function getbowtied_render_frontend_lookbook_distortion_product( $attributes ) {
 		                    <div class="gbt_18_text_wrapper">
 		                    	<div class="gbt_18_content_top">
 			                        <h2 style="color:<?php echo $textColor; ?>"><?php echo $product->get_name(); ?></h2>
-			                        <div style="color:<?php echo $textColor; ?>"><?php echo $product->get_short_description(); ?></div>
+			                        <div class="gbt_18_description" style="color:<?php echo $textColor; ?>"><?php echo $product->get_short_description(); ?></div>
 			                    </div>
 			                    <div class="gbt_18_content_bottom">
 			                        <span class="gbt_18_product_price" style="color:<?php echo $textColor; ?>">
