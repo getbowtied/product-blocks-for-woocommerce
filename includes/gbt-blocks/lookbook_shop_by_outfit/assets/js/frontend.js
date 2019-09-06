abf7e94819e503f4bcfa460df89875c74e198dcc
Vanesa Reinerth <vanesa@getbowtied.com>
2019-09-06 15:01:16 +0300

    lookbook revert full height fixes - will be moved in theme

--- includes/gbt-blocks/lookbook_shop_by_outfit/assets/js/frontend.js
+++ includes/gbt-blocks/lookbook_shop_by_outfit/assets/js/frontend.js
@@ -192,20 +192,4 @@
 		gbt_18_lookbook_pagination();
 	});
 
-	$('.gbt_18_snap_look_book .gbt_18_hero_look_book_item').each(function() {
-
-		if( $(this).parent().is(':first-child') ) {
-
-			var windowHeight = $(window).height();
-			var offsetTop = $(this).offset().top;
-			var fullHeight = 100-offsetTop/(windowHeight/100);
-
-			if( windowHeight && fullHeight ) {
-				$(this).css('min-height', fullHeight+"vh");
-				$(this).css('max-height', fullHeight+"vh");
-			}
-		}
-	});
-
-
 }( jQuery ) );
