7ef258da59f59bf9fa8c4d6f750d203520b653b3
Vanesa Reinerth <vanesa@getbowtied.com>
2020-03-31 20:46:22 +0300

    v.1.3.9

--- README.txt
+++ README.txt
@@ -2,8 +2,8 @@
 Contributors: getbowtied, vanesareinerth, traians, adrianlbs
 Tags: woocommerce, product, blocks, block, store, gutenberg
 Requires at least: 5.0
-Tested up to: 5.3.2
-Stable tag: 1.3.8
+Tested up to: 5.4
+Stable tag: 1.3.9
 Requires PHP: 5.5.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -52,6 +52,9 @@ The manual installation method involves downloading the plugin and uploading it 
 6. Scattered Product List
 
 == Changelog ==
+
+= 1.3.9 =
+* Retested for compatibility with WordPress 5.4 and WooCommerce 4.0.1
 
 = 1.3.8 =
 * Retested for compatibility with WooCommerce 4.0.0
--- product-blocks-for-woocommerce.php
+++ product-blocks-for-woocommerce.php
@@ -4,15 +4,15 @@
  * Plugin Name:       		Product Blocks for WooCommerce
  * Plugin URI:        		https://github.com/getbowtied/product-blocks-for-woocommerce
  * Description:       		Advanced Blocks for WooCommerce.
- * Version:           		1.3.8
+ * Version:           		1.3.9
  * Author:            		GetBowtied
  * Author URI:        		https://getbowtied.com
  * License: 				GPLv2
  * License URI: 			http://www.gnu.org/licenses/gpl-2.0.html
  * Requires at least: 		5.0
- * Tested up to: 			5.3.2
+ * Tested up to: 			5.4
  * WC requires at least: 	3.5
- * WC tested up to: 		4.0.0
+ * WC tested up to: 		4.0.1
  *
  * @package  Product Blocks for WooCommerce
  * @author   GetBowtied
