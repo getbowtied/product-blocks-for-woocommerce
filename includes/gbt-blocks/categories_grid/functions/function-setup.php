888f2327b8b3cfd41a232c7e4ca8a703fa809f76
rthvanesa <vanesa.reinerth@gmail.com>
2021-07-08 15:19:00 +0300

    Categories Grid - Use ServerSide for backend

--- includes/gbt-blocks/categories_grid/functions/function-setup.php
+++ includes/gbt-blocks/categories_grid/functions/function-setup.php
@@ -38,10 +38,10 @@ function pbfw_categories_grid_editor_assets() {
 	);
 
 	wp_register_style(
-		'getbowtied-categories-grid-editor-styles',
-		plugins_url( 'assets/css/backend/editor'.PBFW_SUFFIX.'.css', dirname(__FILE__) ),
-		array(),
-		filemtime(plugin_dir_path( dirname(__FILE__) ) . 'assets/css/backend/editor'.PBFW_SUFFIX.'.css')
+		'getbowtied-categories-grid-styles',
+		plugins_url( 'assets/css/frontend/style'.PBFW_SUFFIX.'.css', dirname(__FILE__) ),
+		array( 'wp-edit-blocks' ),
+		filemtime(plugin_dir_path( dirname(__FILE__) ) . 'assets/css/frontend/style'.PBFW_SUFFIX.'.css')
 	);
 }
 
@@ -50,41 +50,69 @@ function pbfw_categories_grid_editor_assets() {
  */
 register_block_type( 'getbowtied/categories-grid', array(
 	'editor_script'		=> 'getbowtied-categories-grid-editor-scripts',
-	'editor_style'		=> 'getbowtied-categories-grid-editor-styles',
+	'editor_style'		=> 'getbowtied-categories-grid-styles',
 	'script'			=> 'getbowtied-categories-grid-scripts',
 	'style'				=> 'getbowtied-categories-grid-styles',
 	'attributes'      	=> array(
-		'queryDisplayType'				=> array(
-			'type'						=> 'string',
-			'default'					=> 'all_categories',
-		),
 		'categoryIDs'					=> array(
 			'type'						=> 'string',
 			'default'					=> '',
 		),
-		'orderby' 						=> array(
+		'queryCategories' 				=> array(
 			'type'						=> 'string',
-			'default'					=> 'menu_order',
+			'default'					=> '',
 		),
-		'limit'							=> array(
-			'type'						=> 'integer',
-			'default'					=> 8,
+		'queryCategoriesLast' 			=> array(
+			'type'						=> 'string',
+			'default'					=> '',
+		),
+		'queryDisplayType'				=> array(
+			'type'						=> 'string',
+			'default'					=> 'all_categories',
+		),
+		'isLoading'						=> array(
+			'type'    					=> 'boolean',
+			'default' 					=> false,
+		),
+		'querySearchString'  			=> array(
+			'type'						=> 'string',
+			'default'					=> '',
+		),
+		'querySearchNoResults'			=> array(
+			'type'    					=> 'boolean',
+			'default' 					=> false,
+		),
+		'queryOrder' 					=> array(
+			'type'						=> 'string',
+			'default'					=> '',
+		),
+		'parentOnly'					=> array(
+			'type'						=> 'boolean',
+			'default'					=> false,
 		),
 		'hideEmpty'  					=> array(
 			'type'    					=> 'boolean',
 			'default' 					=> false,
 		),
+		'orderby' 						=> array(
+			'type'						=> 'string',
+			'default'					=> 'menu_order',
+		),
 		'productCount'  				=> array(
 			'type'    					=> 'boolean',
 			'default' 					=> true,
 		),
+		'firstLoad' 					=> array(
+			'type'    					=> 'boolean',
+			'default' 					=> true,
+		),
+		'limit'							=> array(
+			'type'						=> 'integer',
+			'default'					=> 8,
+		),
 		'columns'						=> array(
 			'type'						=> 'number',
 			'default'					=> '3'
-		),
-		'parentOnly'					=> array(
-			'type'						=> 'boolean',
-			'default'					=> false,
 		),
 		'align'							=> array(
 			'type'						=> 'string',
